7e4fdb2da248dfe3b296164bca187b83d30bdab6
nils.cc <mail@nils.cc>
2021-11-05 16:50:48 +0100

    Prepare colors module for rich text color parsing

--- lib/relay/colors.dart
+++ lib/relay/colors.dart
@@ -3,15 +3,13 @@
 // https://github.com/ubergeek42/weechat-android/blob/379f0863e9eef70d83462a5d13e8de932eb785b5/relay/src/main/java/com/ubergeek42/weechat/Color.java
 // https://github.com/weechat/weechat/blob/12be3b8c332c75a398f77478fd8d62304c632a1e/src/gui/gui-color.h
 
+import 'package:flutter/cupertino.dart';
+
 const _attributes = ['F', 'B', '*', '!', '/', '_', '|'];
 const _combiners = [',', '~'];
 
-String stripColors(String raw) {
+RichText parseColors(String raw) {
   final List<int> i = [];
-
-  bool colorCode = false;
-  // bool setAttribute = false;
-  // bool remAttribute = false;
 
   final it = raw.runes.iterator;
   while (it.moveNext()) {
@@ -20,43 +18,49 @@ String stripColors(String raw) {
       continue;
     }
 
-    if (it.current == 0x1C)
-      continue;
+    if (it.current == 0x1C) continue;
 
     if (it.current == 0x19) {
-      colorCode = true;
-      continue;
-    }
+      it.moveNext();
 
-    if (colorCode) {
-      if (_attributes.contains(it.currentAsString)) {}
-      else if (it.currentAsString == '@') {
-        // extended: move 5 characters
-        it.moveNext();
-        it.moveNext();
-        it.moveNext();
-        it.moveNext();
-        it.moveNext();
-        it.moveNext();
-        if (!_combiners.contains(it.currentAsString)) {
-          it.movePrevious();
-          colorCode = false;
-        }
-      } else {
-        // standard: move 2 characters
-        it.moveNext();
-        it.moveNext();
-        if (!_combiners.contains(it.currentAsString)) {
-          it.movePrevious();
-          colorCode = false;
-        }
+      // skip attributes
+      while (true) {
+        if
+        (_attributes.contains(it.currentAsString))
+          it.moveNext();
+        else
+          break;
       }
 
-      continue;
+      while (true) {
+        if (it.currentAsString == '@') {
+          it.moveNext(); // skip @
+          // extended: move 5 characters
+          it.moveNext();
+          it.moveNext();
+          it.moveNext();
+          it.moveNext();
+          it.moveNext();
+        } else {
+          // standard: move 2 characters
+          it.moveNext();
+          it.moveNext();
+        }
+
+        // peek next character to check for combiners
+        if (_combiners.contains(it.currentAsString))
+          it.moveNext();
+        else
+          break;
+      }
     }
 
     i.add(it.current);
   }
 
-  return String.fromCharCodes(i);
+  return RichText(text: TextSpan(text: String.fromCharCodes(i)));
+}
+
+String stripColors(String raw) {
+  return parseColors(raw).text.toPlainText();
 }
