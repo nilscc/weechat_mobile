9672311cb4cbacf482c7ed5f7ad17fdf0e8d0077
nils.cc <mail@nils.cc>
2023-02-24 23:40:03 +0100

    Show file picker button

--- lib/pages/settings/ssh_upload_settings.dart
+++ lib/pages/settings/ssh_upload_settings.dart
@@ -1,3 +1,4 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:weechat/pages/settings/config.dart';
@@ -21,9 +22,18 @@ class _SshUploadSettingsState extends State<SshUploadSettings> {
         Container(
           padding: const EdgeInsets.symmetric(vertical: 20),
           child: const ListTile(
-            title: Text('SSH'),
+            title: Text('SSH'), // TODO: add localization
             subtitle: Text('Upload files to SSH'),
           ),
+        ),
+        TextButton(
+          onPressed: () async {
+            final result = await FilePicker.platform.pickFiles();
+            if (result != null) {
+              print(result);
+            }
+          },
+          child: Text('Load SSH key'),
         ),
       ],
     );
