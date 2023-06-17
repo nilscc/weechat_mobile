c093ad1895d5249133b2fe7f27a9da73714cd38e
nils.cc <mail@nils.cc>
2023-06-18 00:17:12 +0200

    Some generated files from flutter

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=E:\dev\flutter"
+export "FLUTTER_APPLICATION_PATH=E:\dev\weechat"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.24.3"
+export "FLUTTER_BUILD_NUMBER=12"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
