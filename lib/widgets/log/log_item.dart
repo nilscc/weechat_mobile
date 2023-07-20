2c33232749048ec8d146d4d7caf8fa9f219aedda
nils.cc <mail@nils.cc>
2023-07-20 22:38:26 +0200

    Rich log formatting and copy to clipboard

--- lib/widgets/log/log_item.dart
+++ lib/widgets/log/log_item.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
 import 'package:weechat/pages/log/event_logger.dart';
 
@@ -15,23 +17,49 @@ class LogItem extends StatelessWidget {
     super.key,
   });
 
-  String _truncate(String message) {
-    var lines = message.split("\n");
+  Widget _truncate(String message) {
+    final lines = message.split("\n");
     final l = lines.length;
     if (truncate != null && l > truncate!) {
-      lines = [
-        ...lines.take((truncate!/2).round()),
-        "",
-        "<< truncated ${l - truncate!} lines >>",
-        "",
-        ...lines.skip(l - (truncate!/2).round()),
-      ];
+      return Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(lines.take((truncate! / 2).round()).join("\n")),
+          Text.rich(
+            TextSpan(text: "\n<< truncated ${l - truncate!} lines >>\n"),
+            style: const TextStyle(color: Colors.grey),
+          ),
+          Text(lines.skip(l - (truncate! / 2).round()).join("\n")),
+        ],
+      );
+    } else {
+      return Text(message);
     }
-    return lines.join("\n");
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    return Text('$dateTime[$logType]\n${_truncate(message)}\n');
+    final messenger = ScaffoldMessenger.of(context);
+
+    return GestureDetector(
+      onLongPress: () async {
+        await Clipboard.setData(ClipboardData(text: message));
+        messenger.showSnackBar(
+          const SnackBar(
+            content: Text("Copied log message to clipboard."),
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text.rich(TextSpan(text: "$dateTime [$logType]", style: const TextStyle(color: Colors.grey))),
+            _truncate(message),
+          ],
+        ),
+      ),
+    );
   }
 }
