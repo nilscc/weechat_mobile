0b14492aee4f2627c3d8e260da214fdff7e0507e
nils.cc <mail@nils.cc>
2023-07-20 22:03:49 +0200

    Fix truncate

--- lib/pages/log.dart
+++ lib/pages/log.dart
@@ -14,6 +14,28 @@ class LogPage extends StatefulWidget {
 
 class _State extends State<LogPage> {
   LogType logType = LogType.INFO;
+  int? truncate;
+
+  static const truncateDropdownValues = [
+    DropdownMenuEntry(value: 20, label: "20"),
+    DropdownMenuEntry(value: 50, label: "50"),
+    DropdownMenuEntry(value: null, label: "None"),
+  ];
+
+  String _truncate(String message) {
+    var lines = message.split("\n");
+    final l = lines.length;
+    if (truncate != null && l > truncate!) {
+      lines = [
+        ...lines.take((truncate!/2).round()),
+        "",
+        "<< truncated ${l - truncate!} lines >>",
+        "",
+        ...lines.skip(l - (truncate!/2).round()),
+      ];
+    }
+    return lines.join("\n");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +49,15 @@ class _State extends State<LogPage> {
           children: [
             Row(
               children: [
+                DropdownMenu<int?>(
+                  dropdownMenuEntries: truncateDropdownValues,
+                  initialSelection: truncateDropdownValues.first.value,
+                  label: const Text("Truncate"),
+                  onSelected: (val) => setState(() {
+                    truncate = val;
+                  }),
+                ),
+                const Spacer(),
                 DropdownMenu(
                   label: const Text('Log Level'),
                   dropdownMenuEntries: LogType.values.reversed
@@ -52,7 +83,7 @@ class _State extends State<LogPage> {
                       .messages
                       .reversed
                       .where((element) => element.item2 >= logType)
-                      .map((e) => Text('${e.item1} [${e.item2}]\n${e.item3}\n'))
+                      .map((e) => Text('${e.item1} [${e.item2}]\n${_truncate(e.item3)}\n'))
                       .toList(),
                 ),
               ),
