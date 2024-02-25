21e9e046496db0a32b777fcb81ec80362e7c1990
nils <mail@nils.cc>
2024-02-25 21:04:38 +0100

    Fix line hdata for weechat >= 4.1 (#38)

--- lib/relay/protocol/line_data.dart
+++ lib/relay/protocol/line_data.dart
@@ -27,15 +27,15 @@ class LineData {
 }
 
 const lineDataSelected =
-    'buffer,date,date_printed,displayed,notify_level,highlight,tags_array,prefix,message';
+    'buffer,date,date_usec,date_printed,date_usec_printed,displayed,notify_level,highlight,tags_array,prefix,message';
 
 List<LineData> handleLineData(
   RelayHData hdata,
   int lineDataPointerPPathIndex,
 ) {
   // hdata has format:
-  // buffer,date,date_printed,displayed,notify_level,highlight,tags_array,prefix,message
-  // 0      1    2            3         4            5         6          7      8
+  // buffer,date,date_usec,date_printed,date_usec_printed,displayed,notify_level,highlight,tags_array,prefix,message
+  // 0      1    2         3            4                 5         6            7         8          9      10
 
   final List<LineData> l = [];
   for (int i = 0; i < hdata.count; ++i) {
@@ -43,12 +43,12 @@ List<LineData> handleLineData(
     final bufferPointer = o.values[0];
     final date = DateTime.fromMillisecondsSinceEpoch(o.values[1] * 1000);
     final datePrinted = DateTime.fromMillisecondsSinceEpoch(o.values[2] * 1000);
-    final displayed = (o.values[3] as String).codeUnits[0] == 1;
-    final notifyLevel = (o.values[4] as String).codeUnits[0];
-    final highlight = (o.values[5] as String).codeUnits[0] == 1;
-    final tags = (o.values[6] as List).map((e) => e as String).toList();
-    final prefix = o.values[7];
-    final message = o.values[8];
+    final displayed = (o.values[5] as String).codeUnits[0] == 1;
+    final notifyLevel = (o.values[6] as String).codeUnits[0];
+    final highlight = (o.values[7] as String).codeUnits[0] == 1;
+    final tags = (o.values[8] as List).map((e) => e as String).toList();
+    final prefix = o.values[9];
+    final message = o.values[10];
 
     l.add(LineData(
       lineDataPointer: o.pPath[lineDataPointerPPathIndex],
