41f4fa0e2933d39eea601f3b6f1c7efe5913edc6
Nils <mail@nils.cc>
2024-10-04 15:15:44 +0200

    use hdata keys instead of hard integer indexes on line data objects

--- lib/relay/protocol/line_data.dart
+++ lib/relay/protocol/line_data.dart
@@ -33,22 +33,31 @@ List<LineData> handleLineData(
   RelayHData hdata,
   int lineDataPointerPPathIndex,
 ) {
-  // hdata has format:
-  // buffer,date,date_usec,date_printed,date_usec_printed,displayed,notify_level,highlight,tags_array,prefix,message
-  // 0      1    2         3            4                 5         6            7         8          9      10
+  // get key-index-pairs for all entries
+  final Map<String, int> keys =
+      (hdata.keys ?? []).asMap().map((i, val) => MapEntry(val.name, i));
 
   final List<LineData> l = [];
   for (int i = 0; i < hdata.count; ++i) {
     final o = hdata.objects[i];
-    final bufferPointer = o.values[0];
-    final date = DateTime.fromMillisecondsSinceEpoch(o.values[1] * 1000);
-    final datePrinted = DateTime.fromMillisecondsSinceEpoch(o.values[2] * 1000);
-    final displayed = (o.values[5] as String).codeUnits[0] == 1;
-    final notifyLevel = (o.values[6] as String).codeUnits[0];
-    final highlight = (o.values[7] as String).codeUnits[0] == 1;
-    final tags = (o.values[8] as List).map((e) => e as String).toList();
-    final prefix = o.values[9];
-    final message = o.values[10];
+
+    // get members
+    final bufferPointer = o.values[keys["buffer"]!];
+    final date =
+DateTime.fromMillisecondsSinceEpoch(o.values[keys["date"]!] * 1000);
+    final datePrinted = DateTime.fromMillisecondsSinceEpoch(
+o.values[keys["date_printed"]!] * 1000);
+    final displayed =
+(o.values[keys["displayed"]!] as String).codeUnits[0] == 1;
+    final notifyLevel =
+(o.values[keys["notify_level"]!] as String).codeUnits[0];
+    final highlight =
+(o.values[keys["highlight"]!] as String).codeUnits[0] == 1;
+    final tags = (o.values[keys["tags_array"]!] as List)
+.map((e) => e as String)
+.toList();
+    final prefix = o.values[keys["prefix"]!];
+    final message = o.values[keys["message"]!];
 
     l.add(LineData(
       lineDataPointer: o.pPath[lineDataPointerPPathIndex],
